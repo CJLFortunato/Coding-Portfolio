f3b7751c5759c54e84a03c3ed846730e083b199d
CJL Fortunato <86192989+CJLFortunato@users.noreply.github.com>
2021-06-25 16:08:38 +0200

    Finish dropdown button for homepage

--- Scripts/dropdown.js
+++ Scripts/dropdown.js
@@ -1,19 +1,35 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
+const toggleInput = document.getElementById("drop-btn");
+let isDropdownOpened = false;
+toggleInput.onclick = dropdownFunc;
+
+
 function dropdownFunc() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").style.display = 'block';
+    isDropdownOpened = true;
+    console.log(toggleInput);
   }
   
   // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      const dropdowns = document.getElementsByClassName("dropdown-content");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        const openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
+  // window.onclick = function(event) {
+  //   if (!event.target.matches('.portrait')) {
+  //     // document.getElementById("myDropdown").style.display = 'none';
+  //     var dropdowns = document.getElementsByClassName("dropdown-content");
+  //     console.log(dropdowns);
+  //     var i;
+  //     for (i = 0; i < dropdowns.length; i++) {
+  //       var openDropdown = dropdowns[i];
+  //       if (openDropdown.style.display = 'block') {
+  //         openDropdown.style.display = 'none';
+  //       }
+  //     }
+  //   }
+  // }
+function closeDropdown() {
+  if (isDropdownOpened === true) {
+    document.getElementById("myDropdown").style.display = 'none';
   }
+}
+
+document.getElementById("close-btn").onclick = closeDropdown;
