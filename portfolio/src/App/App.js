98a8ee889593da9a7bb3583f9b96e6b0973c7e6c
CJL Fortunato <clemence.fortunato@gmail.com>
2021-10-27 16:19:32 +0200

    Add new file structure

---
+++ portfolio/src/App/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./reset.css";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+      <nav>
+            <ul>
+                <li><a href="">Introduction</a></li> 
+                <li><a href="">Background</a></li>
+                <li><a href="">Skills</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Contact</a></li>
+            </ul> 
+        </nav>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        <p>Site built by CJL Fortunato. For more details see <a href="">the Projects section</a>.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
