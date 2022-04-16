57e28f70f2e6ffb66a5baf300027392ecd121e95
CJL Fortunato <clemence.fortunato@gmail.com>
2022-04-16 11:02:03 +0200

    Attempt react again

---
+++ portfolio/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>Clémence Fortunato</h4>
+        <nav>
+          <p><a href=""></a></p>
+          <p><a href=""></a></p>
+          <p><a href=""></a></p>
+        </nav>
+      </header>
+      <main>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Quas quidem, praesentium veniam at adipisci neque, rem rerum quam totam ad nulla accusamus velit, 
+        officia vitae repudiandae vero odio ut? Dolorem!
+      </main>
+      <footer>
+        <p>Made by Clémence Fortunato.
+          <a href="">clemence.fortunato@gmail.com</a>
+          <a href="">GitHub</a>
+          <a href="">LinkedIn</a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/Components/Presentation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Presentation() {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+
+export default Presentation;
