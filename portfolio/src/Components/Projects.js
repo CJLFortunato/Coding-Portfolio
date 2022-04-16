b5c5d8c3f0d89ed66a0c03515f8517cdc99f9245
CJL Fortunato <clemence.fortunato@gmail.com>
2022-04-16 15:29:23 +0200

    Implement carousel in project

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -1,76 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-function Projects() {
-    return ( 
-        <div className="presentation">
-            <h1>Hello World</h1>
-            <p>Tous les projets que j'ai réalisé pour monter en compétences.</p>
+function Projects( { children }) {
+    const [activeIndex, setActiveIndex] = useState(0);
 
-      <div class="projects-grid-container">
+    const updateIndex = (newIndex) => {
+        const nbCards = document.getElementsByClassName("project-card").length;
+        console.log(nbCards);
+        if (newIndex < 0) {
+            newIndex = 0
+        } else if (newIndex >= nbCards) {
+            newIndex = nbCards -1;
+        }
 
-        <div class="project-card left">
-          <img src="./Ressources/project_portfolio.jpg" class="project-img-left" />
-          <div class="project-card-img-p-ctn">
-              <h3>Portfolio</h3>
-              <p>Ce site! C'est une simple page HTML avec un peu de CSS, mais la façon dont elle est structurée peut vous intéresser.</p>
-                <div class="link-ctn">
-                  <a href="" class="deployed">Vous voyez le résultat!</a>
-                  <a href="https://github.com/CJLFortunato/Coding-Portfolio" target="_blank" class="github">GitHub</a>
+        setActiveIndex(newIndex);
+    }
+
+    return (
+        <div className="projects">
+            <h1>Projets personels</h1>
+
+            <div className="projects-container">
+                <button onClick={() => updateIndex(activeIndex - 1)}><i className="fa-solid fa-angle-left"></i></button>
+                <div className="carousel">
+                    <div className="inner" style={{ transform: `translateX(-${activeIndex * 25}%)` }}>
+
+                        <div className="project-card">
+                            <img src="./Ressources/project_portfolio.jpg" className="project-img-left" />
+                            <div className="project-card-img-p-ctn">
+                                <h3>Portfolio</h3>
+                                <p>Ce site! C'est une simple page HTML avec un peu de CSS, mais la façon dont elle est structurée peut vous intéresser.</p>
+                                <div className="link-ctn">
+                                    <a href="" className="deployed">Vous voyez le résultat!</a>
+                                    <a href="https://github.com/CJLFortunato/Coding-Portfolio" target="_blank" className="github">GitHub</a>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="project-card">
+                            <img src="./Ressources/project_dice.jpg" className="project-img-right" />
+                            <div className="project-card-img-p-ctn">
+                                <h3>Lanceur de dés</h3>
+                                <p>Un site simple qui vous permet de lancer des dés, avec un focus sur les jeux de rôle sur table.
+                                    Utile si vous avez oublié vos dés ou que vous n'en avez pas assez pour déterminer les dégâts causés par la boule de feu que vous venez de lancer.</p>
+                                <div className="link-ctn">
+                                    <a href="https://cjlfortunato.github.io/Dice-roller/" target="_blank" className="deployed">Voir site</a>
+                                    <a href="https://github.com/CJLFortunato/Dice-roller" target="_blank" className="github">GitHub</a>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="project-card">
+                            <img src="./Ressources/project_goblin.jpg" className="project-img-left" />
+                            <div className="project-card-img-p-ctn">
+                                <h3>Goblin market</h3>
+                                <p>Ce projet a été réalisé en tant que projet final pour le parcours professionel Front-end Engineer du site Codecademy.
+                                    Il s'agit d'un site d'e-commerce avec un twist <em>fantasy</em>. Seule la partie Front-end a été réalisée, il n'y a pas de back-end. Les principales fonctionalités sont là,
+                                    mais il s'agit d'un work in progress. Réalisé avec React et redux.</p>
+
+                                <div className="link-ctn">
+                                    <a href="https://goblin-market.netlify.app" target="_blank" className="deployed">Voir site</a>
+                                    <a href="https://github.com/CJLFortunato/Goblin-Market" target="_blank" className="github">GitHub</a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <div className="project-card">
+                            <img src="./Ressources/project_jammming.jpg" className="project-img-right" />
+                            <div className="project-card-img-p-ctn">
+                                <h3>Jammming</h3>
+                                <p>Ce site est un projet Codecademy basé sur l'API du site Spotify.
+                                    Il permet à l'utilisateur de chercher des pistes de chansons et de les ajouter à une nouvelle playlist sur leur compte.
+                                    Réalisé avec React.</p>
+
+                                <div className="link-ctn">
+                                    <a href=" http://react-jammming-music.surge.sh/" target="_blank" className="deployed">Voir site</a>
+                                    <a href="https://github.com/CJLFortunato/Jammming" target="_blank" className="github">GitHub</a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              
-          </div>
-        </div>
-
-        <div class="project-card right">
-          <img src="./Ressources/project_dice.jpg" class="project-img-right" />
-          <div class="project-card-img-p-ctn">
-            <h3>Lanceur de dés</h3>
-            <p>Un site simple qui vous permet de lancer des dés, avec un focus sur les jeux de rôle sur table.
-            Utile si vous avez oublié vos dés ou que vous n'en avez pas assez pour déterminer les dégâts causés par la boule de feu que vous venez de lancer.</p>
-            <div class="link-ctn">
-              <a href="https://cjlfortunato.github.io/Dice-roller/" target="_blank" class="deployed">Voir site</a>
-              <a href="https://github.com/CJLFortunato/Dice-roller" target="_blank" class="github">GitHub</a> 
+                <button onClick={() => updateIndex(activeIndex + 1)}><i className="fa-solid fa-angle-right"></i></button>
             </div>
-            
-          </div>
-        </div>
 
-        <div class="project-card left">
-           <img src="./Ressources/project_goblin.jpg" class="project-img-left" />
-          <div class="project-card-img-p-ctn">
-            <h3>Goblin market</h3>           
-            <p>Ce projet a été réalisé en tant que projet final pour le parcours professionel Front-end Engineer du site Codecademy.
-            Il s'agit d'un site d'e-commerce avec un twist <em>fantasy</em>. Seule la partie Front-end a été réalisée, il n'y a pas de back-end. Les principales fonctionalités sont là, 
-            mais il s'agit d'un work in progress. Réalisé avec React et redux.</p>
-            
-            <div class="link-ctn">
-              <a href="https://goblin-market.netlify.app" target="_blank" class="deployed">Voir site</a>
-              <a href="https://github.com/CJLFortunato/Goblin-Market" target="_blank" class="github">GitHub</a>   
-            </div>
-            
-          </div>
-          
-        </div>
-        <div class="project-card right">
-          <img src="./Ressources/project_jammming.jpg" class="project-img-right" />
-          <div class="project-card-img-p-ctn">
-            <h3>Jammming</h3>
-            <p>Ce site est un projet Codecademy basé sur l'API du site Spotify. 
-            Il permet à l'utilisateur de chercher des pistes de chansons et de les ajouter à une nouvelle playlist sur leur compte.
-            Réalisé avec React.</p>
-            
-            <div class="link-ctn">
-              <a href=" http://react-jammming-music.surge.sh/" target="_blank" class="deployed">Voir site</a>
-              <a href="https://github.com/CJLFortunato/Jammming" target="_blank" class="github">GitHub</a>  
-            </div>
-            
-          </div>
-        </div>
 
-      </div>
         </div>
-     );
+    );
 }
 
 export default Projects;
--- portfolio/src/Components/Skills.js
+++ portfolio/src/Components/Skills.js
@@ -3,70 +3,78 @@ import React from 'react';
 
 function Skills() {
     return ( 
-        <div className="presentation">
+        <div className="skills">
             <h1>Skills</h1>
-            <div class="skills-container">
+            
         <p>Voici toutes les compétences que j'ai acquis. Cette section va certainement s'agrandir, puisque je suis toujours en formation!</p>
-        <div class="skills-grid-container">
-          <div class="skill-card one-line">
-            <p><i class="fas fa-code"></i></p>
+        <div className="skills-grid-container">
+          <div className="skill-card one-line">
+            <p><i className="fas fa-code"></i></p>
             <p>HTML5</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="fab fa-css3"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="fab fa-css3"></i></p>
             <p>CSS3</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="fab fa-js"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="fab fa-js"></i></p>
             <p>JavaScript</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="fab fa-react"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="fab fa-react"></i></p>
             <p>React</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="fas fa-atom"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="fas fa-atom"></i></p>
             <p>Redux</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="fab fa-angular"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="fab fa-angular"></i></p>
             <p>Angular</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="fab fa-php"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="fab fa-php"></i></p>
             <p>PHP</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="fas fa-database"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="fas fa-database"></i></p>
             <p>SQL</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="fab fa-git-alt"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="fas fa-database"></i></p>
+            <p>NodeJS</p>
+          </div>
+          <div className="skill-card one-line">
+            <p><i className="fas fa-database"></i></p>
+            <p>MongoDB</p>
+          </div>
+          <div className="skill-card one-line">
+            <p><i className="fab fa-git-alt"></i></p>
             <p>Git et GitHub</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="fas fa-palette"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="fas fa-palette"></i></p>
             <p>Design Visuel</p>
           </div>
-          <div class="skill-card">
-            <p><i class="fas fa-crop"></i></p>
+          <div className="skill-card">
+            <p><i className="fas fa-crop"></i></p>
             <p>Manipulation d'images</p>
           </div>
-          <div class="skill-card">
-            <p><i class="fab fa-accessible-icon"></i></p>
+          <div className="skill-card">
+            <p><i className="fab fa-accessible-icon"></i></p>
             <p>Standards d'accessibilité</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="far fa-comments"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="far fa-comments"></i></p>
             <p>Anglais</p>
           </div>
-          <div class="skill-card one-line">
-            <p><i class="far fa-comments"></i></p>
+          <div className="skill-card one-line">
+            <p><i className="far fa-comments"></i></p>
             <p>Français</p>
           </div>
         </div>
       </div>
-        </div>
+       
      );
 }
 
