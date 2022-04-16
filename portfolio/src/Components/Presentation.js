095a62d8b035877e003b8390a633bbc822a55ba1
CJL Fortunato <clemence.fortunato@gmail.com>
2022-04-16 11:32:22 +0200

    Add presentation component

--- portfolio/src/Components/Presentation.js
+++ portfolio/src/Components/Presentation.js
@@ -3,8 +3,25 @@ import React from 'react';
 
 function Presentation() {
     return ( 
-        <div>
-
+        <div className="presentation">
+            <h1>Hello World</h1>
+            <div className="cards-ctn">
+                <div className='card'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Quas quidem, praesentium veniam at adipisci neque, rem rerum quam totam ad nulla accusamus velit, 
+                officia vitae repudiandae vero odio ut? Dolorem!
+                </div>
+                <div className='card'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Quas quidem, praesentium veniam at adipisci neque, rem rerum quam totam ad nulla accusamus velit, 
+                officia vitae repudiandae vero odio ut? Dolorem!
+                </div>
+                <div className='card'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Quas quidem, praesentium veniam at adipisci neque, rem rerum quam totam ad nulla accusamus velit, 
+                officia vitae repudiandae vero odio ut? Dolorem!
+                </div>
+            </div>
         </div>
      );
 }
