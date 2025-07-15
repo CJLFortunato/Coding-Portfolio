ffd8ceed87e4f9de4f2da98ff10c8c553b9773f2
CJL Fortunato <clemence.fortunato@gmail.com>
2025-07-15 11:45:47 +0200

    Work on pages for projects

---
+++ portfolio/src/app/projects/benhur/page.tsx
@@ -0,0 +1,54 @@
+function BenhurPage () {
+  return (
+    <div className="project">
+      <h1>benhur</h1>
+      <article>
+      <aside>
+        <h2>Stack technique</h2>
+        <h3>Front-end</h3>
+        <ul>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Material UI</li>
+          <li>Recharts</li>
+          <li>Moment</li>
+        </ul>
+        <h3>Back-end</h3>
+        <ul>
+          <li>Python</li>
+          <li>fastAPI</li>
+          <li>SQLAlchemy</li>
+          <li>Pandas</li>
+          <li>postGreSQL</li>
+          <li>Pydantic</li>
+        </ul>
+        <h3>ETL</h3>
+        <ul>
+          <li>Python</li>
+          <li>Apache Airflow</li>
+          <li>Pandas</li>
+        </ul>
+        <h3>Divers</h3>
+        <ul>
+          <li>Google Cloud</li>
+          <li>Docker</li>
+          <li>Jira</li>
+        </ul>
+      </aside>
+      <section>
+        <div className="desc-ctn">
+          <h2>Description</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quaerat at illo, repellat perspiciatis aperiam, laboriosam vitae, molestiae error nisi in.
+          Nam tempore quidem harum repellat maxime, libero sunt reiciendis?</p>
+        </div>
+        <div className="img-ctn">
+          <h2>Images</h2>
+          Images go here
+        </div>
+        </section>
+      </article>
+    </div>
+  );
+}
+
+export default BenhurPage;
--- portfolio/src/app/projects/page.tsx
+++ portfolio/src/app/projects/page.tsx
@@ -1,6 +1,18 @@
+import Link from "next/link";
+
 function ProjectsPage () {
   return (
-    <h1>Projects</h1>
+    <div className="projects">
+      <h1>Projects</h1>
+        <article className="projects-container">
+          <Link href="/projects/benhur">
+          <section className="project-card">
+            <h2>benhur</h2>
+            <p>Outil interne de search listening SEO développé pour l'Agence WAM</p>
+          </section>
+          </Link>
+        </article>
+    </div>
   );
 }
 
