eef16e29c0a5d535e870f24c01f3dc25c7b12c15
CJL Fortunato <clemence.fortunato@gmail.com>
2025-07-31 10:59:50 +0200

    Try to fix deployment error

--- portfolio/netlify/prepare.js
+++ portfolio/netlify/prepare.js
@@ -4,7 +4,7 @@ await fsp
   .rm(".netlify/functions-internal", { recursive: true })
   .catch(() => {});
 await fsp.mkdir(".netlify/functions-internal", { recursive: true });
-await fsp.cp("build/server/", ".netlify/functions-internal/handler", {
+await fsp.cp("build/", ".netlify/functions-internal/handler", {
   recursive: true,
 });
 
